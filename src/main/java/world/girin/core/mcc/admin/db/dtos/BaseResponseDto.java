6966cfb4dbaa2468c840f35362ef98fe834807c2
GiwoongBang <107822628+GiwoongBang@users.noreply.github.com>
2025-01-08 22:04:54 +0900

    feat: BaseResponseDto for error response

---
+++ src/main/java/world/girin/core/mcc/admin/db/dtos/BaseResponseDto.java
@@ -0,0 +1,12 @@
+package world.girin.core.mcc.admin.db.dtos;
+
+import lombok.Getter;
+import lombok.Setter;
+
+@Getter
+@Setter
+public abstract class BaseResponseDto {
+
+    private String errorMessage;
+
+}
